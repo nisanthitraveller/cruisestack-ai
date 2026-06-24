cb63d029b3af621c70a35eb5992ccf2ae00d13e1
sudeep <sudeep.kv@unremot.com>
2026-06-24 14:13:57 +0530

    changes

--- db/migrations/2026-05-15_subscription_plan_stripe_prices.sql
+++ db/migrations/2026-05-15_subscription_plan_stripe_prices.sql
@@ -1,3 +1,24 @@
+ALTER TABLE subscription_plans
+  ADD COLUMN IF NOT EXISTS stripe_price_id varchar(255) DEFAULT NULL;
+
+INSERT INTO subscription_plans (plan_name, stripe_price_id)
+SELECT 'Beginner', 'price_1TXERSEmqvBXj5NHd0ImLbQk'
+WHERE NOT EXISTS (
+  SELECT 1 FROM subscription_plans WHERE plan_name = 'Beginner'
+);
+
+INSERT INTO subscription_plans (plan_name, stripe_price_id)
+SELECT 'Professional', 'price_1TXERoEmqvBXj5NHejeHS6Kk'
+WHERE NOT EXISTS (
+  SELECT 1 FROM subscription_plans WHERE plan_name = 'Professional'
+);
+
+INSERT INTO subscription_plans (plan_name, stripe_price_id)
+SELECT 'Enterprise', 'price_1TXESAEmqvBXj5NHO4QDybcN'
+WHERE NOT EXISTS (
+  SELECT 1 FROM subscription_plans WHERE plan_name = 'Enterprise'
+);
+
 UPDATE subscription_plans
 SET stripe_price_id = 'price_1TXERSEmqvBXj5NHd0ImLbQk'
 WHERE plan_name = 'Beginner';
@@ -8,18 +29,4 @@ WHERE plan_name = 'Professional';
 
 UPDATE subscription_plans
 SET stripe_price_id = 'price_1TXESAEmqvBXj5NHO4QDybcN'
-WHERE plan_name = 'Enterprise';
-
-
-
-UPDATE subscription_plans
-SET stripe_price_id = 'price_1TXERSEmqvBXj5NHd0ImLbQk'
-WHERE plan_name = 'Beginner';
-
-UPDATE subscription_plans
-SET stripe_price_id = 'price_1TXERoEmqvBXj5NHejeHS6Kk'
-WHERE plan_name = 'Professional';
-
-UPDATE subscription_plans
-SET stripe_price_id = 'price_1Tbw2jEmqvBXj5NHQYDYd72b'
 WHERE plan_name = 'Enterprise';
